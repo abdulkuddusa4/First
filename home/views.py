d570491d2630e3f7a1b5ce75287ee7de6de4bced
abdulkuddusa4 <abdulkuddusa4@gmail.com>
2021-01-11 02:15:15 -0500

    a project is added

---
+++ home/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from datetime import datetime
+from home.models import Contact
+from django.contrib import messages
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html') 
+
+def services(request):
+    return render(request, 'services.html')
+ 
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name, email=email, desc=desc, date = datetime.today())
+        contact.save()
+        messages.success(request, 'Your message is succesfully sent to BTS. Congrats!')
+    return render(request, 'contact.html') 
